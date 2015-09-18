22537885b1ceaf81c156bd0ad588eec951c8dac7
ToniRib <tonimarierib@gmail.com>
2015-09-17 23:01:23 -0600

    bug: Insightful console.log statements for bug fixing
    Currently debugging why my map object is created LAST. This commit
    adds a lot of console.log() statements to keep track of which order
    the code is run in.

--- src/js/app.js
+++ src/js/app.js
@@ -1,50 +1,72 @@
 var map;
 
-function initMap() {
-  // Create a map object and specify the DOM element for display.
+// function initMap() {
+//   // Create a map object and specify the DOM element for display.
 
-  map = new google.maps.Map(document.getElementById('google-map'), {
-    center: {
-      lat: 39.679,
-      lng: -104.903
-    },
-    zoom: 13,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+//   map = new google.maps.Map(document.getElementById('google-map'), {
+//     center: {
+//       lat: 39.679,
+//       lng: -104.903
+//     },
+//     zoom: 13,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   });
+
+//   // Dummy marker for testing
+//   var marker = new google.maps.Marker({
+//     position: new google.maps.LatLng(39.6791057, -104.9030243),
+//     map: map,
+//     title: 'Comrade Brewing Co'
+//   });
+// }
+
+var ViewModel = function() {
+  var self = this;
+  console.log('third');
+
+  self.initialize = function() {
+    console.log('map created here!');
+    var mapCanvas = document.getElementById('google-map');
+    var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
+    var mapOptions = {
+      center: cenLatLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(mapCanvas, mapOptions);
+  };
+
+  console.log('fourth');
+  this.breweryList = ko.observableArray([]);
+  breweryLocations.forEach(function(brewItem) {
+    self.breweryList.push( new Brewery(brewItem) );
+    console.log('11');
   });
 
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(39.6791057, -104.9030243),
-    map: map,
-    title: 'Comrade Brewing Co'
-  });
-}
+  console.log('4');
+  google.maps.event.addDomListener(window, 'load', self.initialize);
+
+};
+
+console.log('first');
 
 var Brewery = function(data) {
+  console.log('6');
   this.name = data.name;
   this.lat = data.lat;
   this.lng = data.lng;
   this.address = data.address;
 
   // Google Maps Marker for this location
+  console.log('7');
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(data.lat, data.lng),
     map: map,
     title: this.name
   });
+  console.log('8');
 };
 
-var ViewModel = function() {
-  var self = this;
-
-  google.maps.event.addDomListener(window, 'load', initMap);
-
-  this.breweryList = ko.observableArray([]);
-  breweryLocations.forEach(function(brewItem) {
-    self.breweryList.push( new Brewery(brewItem) );
-  });
-
-
-
-};
-
+console.log('second');
 ko.applyBindings(new ViewModel() );
+console.log('10');
