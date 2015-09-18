897885ce8177696e1585b12c71b47b9ccc0a7b66
ToniRib <tonimarierib@gmail.com>
2015-09-17 23:27:32 -0600

    style: Clean up comments in app.js
    Cleans up the commented out code in app.js and adds useful comments.
    No code changes from the last commit.

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,5 @@
+// Initialize the map
 var map;
-
-// function initMap() {
-//   // Create a map object and specify the DOM element for display.
-
-//   map = new google.maps.Map(document.getElementById('google-map'), {
-//     center: {
-//       lat: 39.679,
-//       lng: -104.903
-//     },
-//     zoom: 13,
-//     mapTypeId: google.maps.MapTypeId.ROADMAP
-//   });
-
-//   // Dummy marker for testing
-//   var marker = new google.maps.Marker({
-//     position: new google.maps.LatLng(39.6791057, -104.9030243),
-//     map: map,
-//     title: 'Comrade Brewing Co'
-//   });
-// }
-
-var ViewModel = function() {
-  var self = this;
-
-  // self.initialize = function() {
-  //   console.log('map created here!');
-  //   var mapCanvas = document.getElementById('google-map');
-  //   var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
-  //   var mapOptions = {
-  //     center: cenLatLng,
-  //     zoom: 13,
-  //     mapTypeId: google.maps.MapTypeId.ROADMAP
-  //   };
-  //   map = new google.maps.Map(mapCanvas, mapOptions);
-  // };
-
-  this.breweryList = ko.observableArray([]);
-  breweryLocations.forEach(function(brewItem) {
-    self.breweryList.push( new Brewery(brewItem) );
-  });
-
-  // google.maps.event.addDomListener(window, 'load', self.initialize);
-
-};
-
 var mapCanvas = document.getElementById('google-map');
 var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
 var mapOptions = {
@@ -53,6 +9,18 @@ var mapOptions = {
 };
 map = new google.maps.Map(mapCanvas, mapOptions);
 
+// Set up the ViewModel
+var ViewModel = function() {
+  var self = this;
+
+  this.breweryList = ko.observableArray([]);
+  breweryLocations.forEach(function(brewItem) {
+    self.breweryList.push( new Brewery(brewItem) );
+  });
+
+};
+
+// Create the brewery objects
 var Brewery = function(data) {
   this.name = data.name;
   this.lat = data.lat;
@@ -67,4 +35,5 @@ var Brewery = function(data) {
   });
 };
 
+// Kick everything off!
 ko.applyBindings(new ViewModel() );
