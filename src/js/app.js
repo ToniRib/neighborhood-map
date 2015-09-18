959b2fc67c13ccf447d16cc5ee850ea8ee4bb0e0
ToniRib <tonimarierib@gmail.com>
2015-09-17 23:21:21 -0600

    feat: Map Markers Work!!!!!
    After many hours, I finally got the map to be the first variable that
    is created. Now all the map markers render exactly where I want them.
    Awesome.

--- src/js/app.js
+++ src/js/app.js
@@ -22,51 +22,49 @@ var map;
 
 var ViewModel = function() {
   var self = this;
-  console.log('third');
 
-  self.initialize = function() {
-    console.log('map created here!');
-    var mapCanvas = document.getElementById('google-map');
-    var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
-    var mapOptions = {
-      center: cenLatLng,
-      zoom: 13,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    map = new google.maps.Map(mapCanvas, mapOptions);
-  };
+  // self.initialize = function() {
+  //   console.log('map created here!');
+  //   var mapCanvas = document.getElementById('google-map');
+  //   var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
+  //   var mapOptions = {
+  //     center: cenLatLng,
+  //     zoom: 13,
+  //     mapTypeId: google.maps.MapTypeId.ROADMAP
+  //   };
+  //   map = new google.maps.Map(mapCanvas, mapOptions);
+  // };
 
-  console.log('fourth');
   this.breweryList = ko.observableArray([]);
   breweryLocations.forEach(function(brewItem) {
     self.breweryList.push( new Brewery(brewItem) );
-    console.log('11');
   });
 
-  console.log('4');
-  google.maps.event.addDomListener(window, 'load', self.initialize);
+  // google.maps.event.addDomListener(window, 'load', self.initialize);
 
 };
 
-console.log('first');
+var mapCanvas = document.getElementById('google-map');
+var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
+var mapOptions = {
+  center: cenLatLng,
+  zoom: 12,
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+map = new google.maps.Map(mapCanvas, mapOptions);
 
 var Brewery = function(data) {
-  console.log('6');
   this.name = data.name;
   this.lat = data.lat;
   this.lng = data.lng;
   this.address = data.address;
 
   // Google Maps Marker for this location
-  console.log('7');
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(data.lat, data.lng),
     map: map,
     title: this.name
   });
-  console.log('8');
 };
 
-console.log('second');
 ko.applyBindings(new ViewModel() );
-console.log('10');
