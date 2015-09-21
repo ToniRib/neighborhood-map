81fd618103279043cf310cbad2b2399b8b1317b1
ToniRib <tonimarierib@gmail.com>
2015-09-21 14:39:51 -0600

    feat: Adds breweries, fixes brewery lat/lng, adds CSS
    Some of the breweries had incorrect location so this commit fixes that.
    Also added Denver Beer Co and Wynkoops. Adds some css styling for the
    list of breweries.

--- src/js/model.js
+++ src/js/model.js
@@ -3,32 +3,32 @@
 var breweryLocations = [
   {
     name: 'Comrade Brewing Company',
-    lat: 39.6791057,
-    lng: -104.9030243,
+    lat: 39.675659,
+    lng: -104.898488,
     address: '7667 E Iliff Ave, Denver, CO 80231'
   },
   {
     name: 'Copper Kettle Brewing Company',
-    lat: 39.6847865,
-    lng: -104.88191,
+    lat: 39.6926811,
+    lng: -104.8913405,
     address: '1338 S Valentia St #100, Denver, CO 80247'
   },
   {
     name: 'Bull & Bush Brewing Co',
-    lat: 396713773,
-    lng: -1048998513,
+    lat: 39.7030711,
+    lng: -104.9324947,
     address: '4700 E Cherry Creek S Dr, Denver, CO 80246'
   },
   {
     name: 'Dry Dock Brewing Co',
-    lat: 39.6663561,
-    lng: -104.8758187,
+    lat: 39.6527382,
+    lng: -104.812003,
     address: '15120 E Hampden Ave, Aurora, CO 80014'
   },
   {
     name: 'Platt Park Brewing Company',
-    lat: 39.6865797,
-    lng: -104.9609628,
+    lat: 39.682588,
+    lng: -104.9808019,
     address: '1875 S Pearl St, Denver, CO 80210'
   },
   {
@@ -39,20 +39,32 @@ var breweryLocations = [
   },
   {
     name: 'Lowdown Brewery',
-    lat: 39.711613,
-    lng: -104.9879888,
+    lat: 39.7293714,
+    lng: -104.9857492,
     address: '800 Lincoln St, Denver, CO 80203'
   },
   {
     name: 'Vine Street Pub & Brewery',
-    lat: 39.7302983,
-    lng: -104.9782985,
+    lat: 39.7434001,
+    lng: -104.9619981,
     address: '1700 Vine St, Denver, CO 80206'
   },
   {
     name: 'Great Divide Brewing Co',
-    lat: 39.7470625,
-    lng: -104.9908298,
+    lat: 39.7539071,
+    lng: -104.9883847,
     address: '2201 Arapahoe St, Denver, CO 80205'
+  },
+  {
+    name: 'Denver Beer Co',
+    lat: 39.7583015,
+    lng: -105.0072748,
+    address: '1695 Platte St, Denver, CO 80202'
+  },
+  {
+    name: 'Wynkoop Brewering Company',
+    lat: 39.7534879,
+    lng: -104.9983932,
+    address: '1634 18th St, Denver, CO 80202'
   }
 ];
