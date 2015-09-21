a29af3ef85e3a40e71de12b8f9ac23c680c82e8e
ToniRib <tonimarierib@gmail.com>
2015-09-21 10:22:09 -0600

    feat: Turns brewery data from model.js into ko observables
    Within the brewery constructor, the data from model.js is now
    knockout observables.

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,8 @@ var map;
 
 var ViewModel = function() {
   var self = this;
-  console.log('third');
 
   self.initialize = function() {
-    console.log('map created here!');
     var mapCanvas = document.getElementById('google-map');
     var cenLatLng = new google.maps.LatLng(breweryLocations[0].lat, breweryLocations[0].lng);
     var mapOptions = {
@@ -16,41 +14,31 @@ var ViewModel = function() {
     map = new google.maps.Map(mapCanvas, mapOptions);
   };
 
-  console.log('fourth');
   self.buildBreweryLocations = function() {
     this.breweryList = ko.observableArray([]);
     breweryLocations.forEach(function(brewItem) {
       self.breweryList.push( new Brewery(brewItem) );
-      console.log('11');
     });
   };
 
-  console.log('4');
   google.maps.event.addDomListener(window, 'load', function() {
     self.initialize();
     self.buildBreweryLocations();
   });
 };
 
-console.log('first');
-
 var Brewery = function(data) {
-  console.log('6');
-  this.name = data.name;
-  this.lat = data.lat;
-  this.lng = data.lng;
-  this.address = data.address;
+  this.name = ko.observable(data.name);
+  this.lat = ko.observable(data.lat);
+  this.lng = ko.observable(data.lng);
+  this.address = ko.observable(data.address);
 
   // Google Maps Marker for this location
-  console.log('7');
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng(data.lat, data.lng),
     map: map,
     title: this.name
   });
-  console.log('8');
 };
 
-console.log('second');
 ko.applyBindings(new ViewModel() );
-console.log('10');
