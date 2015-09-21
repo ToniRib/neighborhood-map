9f972498b840952c6dacde1505876f1be4625b6b
ToniRib <tonimarierib@gmail.com>
2015-09-21 12:43:36 -0600

    feat: Adds InfoWindows
    Adds the infowindow functionality back in (similar to what was on
    the master branch)

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
 var map;
 
+var infoWindow = new google.maps.InfoWindow({
+  content: 'default'
+});
+
 var ViewModel = function() {
   var self = this;
 
+  // Create the google map zoomed in on Denver
   self.initialize = function() {
     var mapCanvas = document.getElementById('google-map');
     var cenLatLng = new google.maps.LatLng(39.716209, -104.940702);
@@ -14,26 +19,29 @@ var ViewModel = function() {
     map = new google.maps.Map(mapCanvas, mapOptions);
   };
 
+  // Create the list of brewery locations from the model
   self.buildBreweryLocations = function() {
     this.breweryList = ko.observableArray([]);
+
     breweryLocations.forEach(function(brewItem) {
       self.breweryList.push( new Brewery(brewItem) );
     });
   };
 
+  // Add the listener for loading the page
   google.maps.event.addDomListener(window, 'load', function() {
     self.initialize();
     self.buildBreweryLocations();
   });
 };
 
+// Brewery constructor to create breweries & marks from the model
 var Brewery = function(data) {
-  var marker;
+  var marker, infoContent;
   this.name = ko.observable(data.name);
   this.lat = ko.observable(data.lat);
   this.lng = ko.observable(data.lng);
   this.address = ko.observable(data.address);
-  this.marker = ko.observable(marker);
 
   // Google Maps Marker for this location
   marker = new google.maps.Marker({
@@ -41,6 +49,16 @@ var Brewery = function(data) {
     map: map,
     title: this.name()
   });
+
+  infoContent = '<div><h4>' + this.name() + '</h4></p>' + this.address() + '</p></div>'
+
+  marker.addListener('click', function() {
+    infoWindow.setContent(infoContent);
+    infoWindow.open(map, marker);
+  });
+
+  // Set the marker as a knockout observables
+  this.marker = ko.observable(marker);
 };
 
 ko.applyBindings(new ViewModel() );
