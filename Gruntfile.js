c2876562c24c9c90e159a968f801385bae03bd34
ToniRib <tonimarierib@gmail.com>
2015-09-22 11:08:25 -0600

    feat: Add Grunt tasks
    Adds the package.json and Gruntfile.js, along with associated node
    modules, to minify both the css and the javascript. Production code
    is now stored in the dist directory.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  // Load grunt tasks
+  require('load-grunt-tasks')(grunt);
+
+  // Grunt configuration tasks
+  grunt.initConfig({
+
+    // Read the package.json file
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Minify the javascript file
+    uglify: {
+      build: {
+        files: {
+          'dist/js/app.min.js': 'src/js/app.js',
+          'dist/js/model.min.js': 'src/js/model.js',
+          'dist/js/mobile.min.js': 'src/js/mobile.js',
+          'dist/js/lib/jQuery.min.js': 'src/js/lib/jQuery.js',
+          'dist/js/lib/knockout-3.2.0.min.js': 'src/js/lib/knockout-3.2.0.js'
+        }
+      }
+    },
+
+    // Minify the CSS files
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src/css/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist/css/',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    // Inline the CSS into the index.html file
+    // inlinecss: {
+    //   main: {
+    //     options: {},
+    //     files: {
+    //       'dist/index.html': 'src/index.html'
+    //     }
+    //   }
+    // },
+  });
+
+  // Tell grunt to use the plugins
+  grunt.loadNpmTasks('grunt-inline-css');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // Register the tasks as default actions for the 'grunt' command
+  grunt.registerTask('default', ['uglify', 'cssmin']);
+
+};
